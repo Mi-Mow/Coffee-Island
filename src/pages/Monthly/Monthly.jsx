e5ea166e08260080bb7aeb8a98ec6b2112632caf
ling-pu <impu317@gmail.com>
2025-04-21 13:22:56 +0800

    0421-1

--- src/pages/Monthly/Monthly.jsx
+++ src/pages/Monthly/Monthly.jsx
@@ -4,6 +4,11 @@ function Monthly() {
   return (
     <>
         <h1>島嶼月報</h1> 
+        <nav>
+          <ul>
+            <li><a href=""></a></li>
+          </ul>
+        </nav>
     </>
   )
 }
