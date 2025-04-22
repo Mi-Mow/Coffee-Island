d8c2d49acd51c955cb1ea66ca188a3165d503e6b
ling-pu <impu317@gmail.com>
2025-04-22 15:40:35 +0800

    0422-3

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import './News.scss'
 import Article from './Article'
+import story1 from '../../assets/news/story1.png'
 
 function News() {
   return (
@@ -10,17 +11,40 @@ function News() {
         <main>
           <nav>
             <div className='nav-news'>
-              <li>
-                <Link to="/news/article">閱讀文章</Link>
-              </li>
-              <li>
-                <Link to="/news/event">找活動</Link>
-              </li>
+              <ul
+              ><li>
+                  <Link to="/news/article">閱讀文章</Link>
+                </li>
+                <li>
+                  <Link to="/news/event">找活動</Link>
+                </li>
+              </ul>
+
             </div>
           </nav>
-          <main>
+          
+          <br />
 
-          </main>
+          <section id='coverstorys'>
+            <img src={story1} alt="封面故事1" />
+          </section>
+
+          <br />
+
+          <section className='section2'>
+            <div className='feature'>
+              <h2>老臺北特輯</h2>
+              <img src={story1} alt="老臺北特輯1" />
+
+            </div>
+            <div className='recommendation'>
+              <h2>人氣推薦</h2>
+
+            </div>
+
+          </section>
+
+
 
         </main>
 
