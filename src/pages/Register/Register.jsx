ca9836a65b8061f08ab9ebad95303868e0888653
Xfinfin <dasper329@gmail.com>
2025-05-02 16:41:27 +0800

    0525

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -9,16 +9,16 @@ function Register() {
                 <h2 className="main-title">免費加入</h2>
 
                 <label className="input-label" htmlFor="email">設定帳號*</label>
-                <input className="login-input" type="email" id="email" placeholder="請輸入電子郵件" required />
+                <input className="register-input" type="email" id="email" placeholder="請輸入電子郵件" required />
 
                 <label className="input-label" htmlFor="password">確認帳號*</label>
-                <input className="login-input" type="password" id="confirm-email" placeholder="請再輸入一次電子郵件" required />
+                <input className="register-input" type="password" id="confirm-email" placeholder="請再輸入一次電子郵件" required />
 
                 <label className="input-label" htmlFor="password">設定密碼*</label>
-                <input className="login-input" type="password" id="password" placeholder="請輸入密碼" required />
+                <input className="register-input" type="password" id="password" placeholder="請輸入密碼" required />
 
                 <label className="input-label" htmlFor="password">確認密碼*</label>
-                <input className="login-input" type="password" id="confirm-password" placeholder="請再輸入一次密碼" required />
+                <input className="register-input" type="password" id="confirm-password" placeholder="請再輸入一次密碼" required />
 
                 <div className="options">
                     <label className="remember">
